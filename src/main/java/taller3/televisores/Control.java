2e9d3af42473990921f2c2d5ce8cfb5455365666
Capry1203 <capry2512@gmail.com>
2023-02-26 00:03:37 -0500

    dios, quiereme una vez por favor

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,38 @@
+package taller3.televisores;
+
+public class Control{
+    private TV tv;
+
+    public TV getTv(){
+        return this.tv;
+    }
+    public void setTv(TV tv){
+        this.tv = tv;
+    }
+
+    public void canalUp(){
+        tv.canalUp();
+    }
+    public void canalDown(){
+        tv.canalDown();
+    }
+//+++++++++++++++++++++++++++++
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+//++++++++++ set Canal +++++++++
+
+    public void setCanal(int canal){
+        tv.setCanal(canal);
+    }
+//++++++++ Enlace +++++++++++++++
+
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+    
+}
